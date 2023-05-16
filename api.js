08f5667c0d0af0170d067f8837e5b8cd341c9988
acato2 <acato1@etf.unsa.ba>
2023-05-16 13:54:01 +0200

    Implement login route

--- api.js
+++ api.js
@@ -1,13 +1,43 @@
 const client = require('./connection.js')
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(cors());
 
-app.listen(3000, ()=>{
-    console.log("Sever is now listening at port 3000");
-})
 
-client.connect();
 
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    const query = `
+      SELECT * FROM users
+      WHERE username = $1 AND password = $2
+      LIMIT 1;
+    `;
+  
+    const values = [username, password];
+  
+    try {
+      const { rows } = await client.query(query, values);
+  
+      if (rows.length === 1) {
+        res.status(200).json({ message: 'Logged in successfully!' });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'An error occurred' });
+    }
+  });
 
 app.get('/users', (req, res)=>{
     client.query(`Select * from users`, (err, result)=>{
@@ -17,3 +47,10 @@ app.get('/users', (req, res)=>{
     });
     client.end;
 })
+
+
+app.listen(3000, ()=>{
+    console.log("Sever is now listening at port 3000");
+})
+
+client.connect();
--- screens/Login.js
+++ screens/Login.js
@@ -10,105 +10,113 @@ const { width, height } = Dimensions.get('window');
 
 
 
-const Login = ({navigation}) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+const Login = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState('');
 
-    /*const navigation = useNavigation();
 
-    const handleButtonPress = () => {
-        navigation.navigate('Registration');
-      }*/
-      
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://10.0.2.2:3000/login', {
+        username: username,
+        password: password,
+      });
 
-    const handleLogin = async () =>{
-        if(!username || !password){
-            alert("Sva polja trebaju biti popunjena!");
-            return;
-        }
-        try{
-            /*const {data} = await axios.post('http://localhost:3000/login',{
-                username,
-                password
-            })*/
-            //console.log("LOGIN SUCCESSFULL ",data);
-            navigation.navigate('HomePage');
-        }
-        catch (err){
-            console.log(err);
-        }
+      if (response.status === 200) {
+        // Logged in successfully
+        setMessage('');
+        navigation.navigate('HomePage');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        // Invalid username or password
+        setMessage('Invalid username or password');
+      } else {
+        // Other errors
+        setMessage('An error occurred');
+      }
     }
+  };
 
-    return (
-        <KeyboardAvoidingWrapper>
-        <View style={styles.container}>
+
+  return (
+    <KeyboardAvoidingWrapper>
+      <View style={styles.container}>
         <View style={styles.login}>
-            <Image source={require("../assets/logo.png")} style={styles.image}></Image>
-            <Text style={styles.textLogin} dark bold>Prijava</Text>
-            <Text style={styles.subtitle} >Prijavi se da nastaviš.</Text>
-            <UserInput name="KORISNIČKO IME" value={username} setValue={setUsername}></UserInput>
-            <UserInput name="LOZINKA" value={password} setValue={setPassword} secureTextEntry={true}></UserInput>
-            <TouchableOpacity style={styles.buttonLogin} onPress={handleLogin}>
-                <Text dark bold style={{color:'#FFFFFF' , fontSize:width*0.04}}>Prijavi se</Text>
-            </TouchableOpacity>
-            <Text bold style={{marginTop:20}}>Nemate račun?</Text>
-            <TouchableOpacity style={styles.buttonRegistration} onPress={()=>navigation.navigate('Registration')}>
-                <Text dark bold style={{color:'#000000' , fontSize:width*0.04}}>Registruj se</Text>
-            </TouchableOpacity>
+          <Image source={require("../assets/logo.png")} style={styles.image}></Image>
+          <Text style={styles.textLogin} dark bold>Prijava</Text>
+          <Text style={styles.subtitle} >Prijavi se da nastaviš.</Text>
+          <UserInput name="KORISNIČKO IME" value={username} setValue={setUsername}></UserInput>
+          <UserInput name="LOZINKA" value={password} setValue={setPassword} secureTextEntry={true}></UserInput>
+          {message ? (
+        <Text style={styles.message}>{message}</Text>
+      ) : null}
+          <TouchableOpacity style={styles.buttonLogin} onPress={handleLogin}>
+            <Text dark bold style={{ color: '#FFFFFF', fontSize: width * 0.04 }}>Prijavi se</Text>
+          </TouchableOpacity>
+          <Text bold style={{ marginTop: 20 }}>Nemate račun?</Text>
+          <TouchableOpacity style={styles.buttonRegistration} onPress={() => navigation.navigate('Registration')}>
+            <Text dark bold style={{ color: '#000000', fontSize: width * 0.04 }}>Registruj se</Text>
+          </TouchableOpacity>
         </View>
-        </View>
-       </KeyboardAvoidingWrapper>
-    )
+      </View>
+    </KeyboardAvoidingWrapper>
+  )
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: width * 0.35,
-        height: width * 0.35,
-        marginTop: width * -0.35,
-        marginBottom: height * 0.02,
-      },
-      container: {
-        flex: 1,
-        backgroundColor: '#00004d',
-      },
-      login: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: height * 0.3,
-        borderTopRightRadius: 0.15 * height
-      },
-      textLogin: {
-        marginTop: 0.01 * height,
-        fontSize: 0.06 * height,
-        color: 'rgb(28,33,32)',
-      },
-      subtitle: {
-        marginBottom: 0.06 * height,
-      },
-      buttonLogin: {
-        backgroundColor: '#00b300',
-        borderRadius: 0.03 * height,
-        paddingVertical: 0.02 * height,
-        paddingHorizontal: 0.08 * width,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 0.7 * width,
-        marginTop: 0.02 * height,
-      },
-      buttonRegistration: {
-        backgroundColor: '#ffdb4d',
-        borderRadius: 0.03 * height,
-        paddingVertical: 0.02 * height,
-        paddingHorizontal: 0.08 * width,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 0.7 * width,
-        marginTop: 0.01 * height,
-        marginBottom:'100%'
-      }
+  image: {
+    width: width * 0.35,
+    height: width * 0.35,
+    marginTop: width * -0.35,
+    marginBottom: height * 0.02,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#00004d',
+  },
+  login: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: height * 0.3,
+    borderTopRightRadius: 0.15 * height
+  },
+  textLogin: {
+    marginTop: 0.01 * height,
+    fontSize: 0.06 * height,
+    color: 'rgb(28,33,32)',
+  },
+  subtitle: {
+    marginBottom: 0.06 * height,
+  },
+  buttonLogin: {
+    backgroundColor: '#00b300',
+    borderRadius: 0.03 * height,
+    paddingVertical: 0.02 * height,
+    paddingHorizontal: 0.08 * width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 0.7 * width,
+    marginTop: 0.02 * height,
+  },
+  buttonRegistration: {
+    backgroundColor: '#ffdb4d',
+    borderRadius: 0.03 * height,
+    paddingVertical: 0.02 * height,
+    paddingHorizontal: 0.08 * width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 0.7 * width,
+    marginTop: 0.01 * height,
+    marginBottom: '100%'
+  },
+  message: {
+    color: 'red',
+    marginTop: 10,
+  }
 });
 
 export default Login;
