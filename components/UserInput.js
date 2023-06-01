cfed04140b93513f7cd8b3f470c7814290bdfa4c
acato2 <acato1@etf.unsa.ba>
2023-06-01 13:32:34 +0200

    Add eye toggle to password input

--- components/UserInput.js
+++ components/UserInput.js
@@ -1,41 +1,65 @@
-import React from "react";
-import { StyleSheet, View, TextInput,Dimensions } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput, Dimensions, TouchableOpacity } from "react-native";
 import Text from '@kaloraat/react-native-text';
+import { Ionicons } from '@expo/vector-icons';
 const { width, height } = Dimensions.get('window');
-const UserInput = ({name, value, setValue, secureTextEntry, autoCapitalize}) => {
-    return (
 
-        <View style={styles.inputData}>
-            <Text semi style={styles.label}>{name}</Text>
-            <TextInput style={styles.inputField} 
-            value = {value}
-            onChangeText = {(text) => setValue(text)}
-            secureTextEntry = {secureTextEntry}
-            autoCapitalize={autoCapitalize}></TextInput>
-        
-        </View>
-    )
-}
+const UserInput = ({ name, value, setValue, secureTextEntry, autoCapitalize }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View style={styles.inputData}>
+      <Text semi style={styles.label}>{name}</Text>
+      <View style={styles.inputFieldContainer}>
+        <TextInput
+          style={styles.inputField}
+          value={value}
+          onChangeText={(text) => setValue(text)}
+          secureTextEntry={!showPassword && secureTextEntry}
+          autoCapitalize={autoCapitalize}
+        />
+        {secureTextEntry && (
+          <TouchableOpacity
+            style={styles.eyeIcon}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Ionicons
+              name={showPassword ? 'md-eye-off' : 'md-eye'}
+              size={24}
+              color="#a6a6a6"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    inputData:{
-        marginHorizontal:24
-    },
-    label:{
-        marginBottom:5,
-        fontSize:width*0.03
-    },
-    inputField: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: 20,
-        borderWidth: 1,
-        borderColor: '#CCCCCC',
-        padding: 10,
-        width: 0.7 * width,
-        marginBottom:10
-
-
-    }
+  inputData: {
+    marginHorizontal: 24,
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: width * 0.03,
+  },
+  inputFieldContainer: {
+    position: 'relative',
+  },
+  inputField: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    padding: 10,
+    width: 0.7 * width,
+    marginBottom: 10,
+  },
+  eyeIcon: {
+    position: 'absolute',
+    top: 13,
+    right: 10,
+  },
 });
 
 export default UserInput;
